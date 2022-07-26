a83003650d2a4af3451579af42bf122421dd5415
João Gabriel Cardoso <joaogabrielrcardoso@gmail.com>
2022-07-26 18:49:53 -0300

    Organização das páginas de receitas atualizada

--- receitas/views.py
+++ receitas/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 
 
 def index_view(request):
-  return render(request, 'index.html')
+  context = {
+    'list': [{
+      'name': 'Sorvete'
+    }]
+  }
+  return render(request, 'index.html', context)
+
 
 def recipe_view(request):
   return render(request, 'receita.html')
