bf7c75f9cb5b53352131febe3807f494dbc13016
João Gabriel Cardoso <joaogabrielrcardoso@gmail.com>
2022-07-28 16:25:12 -0300

    Visualização e criação das proprias receitas adionado

--- receitas/urls.py
+++ receitas/urls.py
@@ -2,11 +2,15 @@ from django.urls import path
 
 from .views import (
   index_view, 
-  recipe_view  
+  recipe_view,
+  recipe_detail_view,
+  recipe_create_view
 )
 
 
 urlpatterns = [
   path('', index_view, name='index'),
-  path('receitas/<int:pk>/', recipe_view, name='recipe')  
+  path('recipes/<int:pk>/', recipe_detail_view, name='recipe'),
+  path('recipes/create/', recipe_create_view, name='recipe_create'),
+  path('recipes/', recipe_view, name='recipes')
 ]
--- receitas/views.py
+++ receitas/views.py
@@ -1,7 +1,49 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
 
 from receitas.models import Recipe
+
+
+User = get_user_model()
+
+@login_required
+def recipe_view(request):  
+  recipe_queryset = Recipe.objects.filter(is_active=True, owner=request.user).order_by('-created_at')  
+  context = {
+    'recipes': recipe_queryset
+  }
+  return render(request, 'pages/index.html', context)
+
+
+@login_required
+def recipe_create_view(request):
+
+  if request.method == 'POST':
+    user = request.user
+    name = request.POST.get('name')
+    ingredients = request.POST.get('ingredients')
+    preparation = request.POST.get('preparation')
+    preparation_time = request.POST.get('preparation_time')
+    potion = request.POST.get('potion')
+    category = request.POST.get('category')    
+    image = request.FILES.get('image')
+
+    recipe = Recipe.objects.create(
+      owner=user, 
+      name=name, 
+      ingredients=ingredients,
+      preparation=preparation,
+      preparation_time=preparation_time,
+      potion=potion,
+      category=category      
+    )
+    if image:          
+      recipe.image = image    
+    recipe.save()
+    return redirect('/')
+
+  return render(request, 'pages/recipe-create.html')
 
 
 @login_required
@@ -17,7 +59,7 @@ def index_view(request):
 
 
 @login_required
-def recipe_view(request, pk=None):
+def recipe_detail_view(request, pk=None):
   recipe_object = get_object_or_404(Recipe, pk=pk)    
   recipe_image = recipe_object.image.url
   if recipe_object.image.name == recipe_object.image.field.default:
